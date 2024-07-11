98f95b490e2e824b9478aaa15b13579a9424aa70
MrKyrylo <138040487+MrKyrylo@users.noreply.github.com>
2024-07-11 18:50:34 +0300

    1

--- src/components/searchBox/searchBox.jsx
+++ src/components/searchBox/searchBox.jsx
@@ -1,22 +1,21 @@
-import css from './searchBox.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { useId } from 'react';
-import { changeFilter, selectNameFilter } from '../../redux/filterSlice';
+import css from "./searchBox.module.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { selectNameFilter } from "../../redux/filtersSlice";
+import { changeFilter } from "../../redux/filtersSlice";
 
 export default function SearchBox() {
-        const dispatch = useDispatch();    
-        const filter = useSelector(selectNameFilter);
+  const nameFilter = useSelector((state) => selectNameFilter(state));
+  const dispatch = useDispatch();
 
-        const filterFieldId = useId();
-        return (
-            <div className={css.filterDiv}>
-            <label htmlFor={filterFieldId}>Find contacts by name</label>
-            <input
-                type="text"
-                id={filterFieldId}
-                value={filter}
-                onChange={(event) => dispatch(changeFilter(event.target.value))}
-            />
-            </div>
-    );
+  function onSearch(event) {
+    dispatch(changeFilter(String(event.target.value)));
+  }
+
+  return (
+    <div className={css.findBox}>
+      <label>Find contacts by name</label>
+      <input type="text" onChange={onSearch} value={nameFilter} />
+    </div>
+  );
 }
